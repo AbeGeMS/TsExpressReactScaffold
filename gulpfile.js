cc75707365aa381adc0dced1a50357603bbd006e
Abe Ge <yige@microsoft.com>
2018-02-26 19:09:29 +0800

    add yarn and modify related files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
-var fs = require("fs");
+var clean = require("gulp-rimraf");
+var flatten = require("gulp-flatten");
 
-gulp.task("build",function(){
+gulp.task("clean", function () {
+    console.log("Clean all files in debug folder");
+    return gulp.src("debug/*", { read: false }).pipe(clean());
+});
+
+gulp.task("build", ["clean"], function () {
+    console.log("build typscript");
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest("debug"));
+        .js.pipe(flatten())
+        .pipe(gulp.dest("debug"));
 });
