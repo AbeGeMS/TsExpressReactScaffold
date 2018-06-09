b8b8a211efdf2a01644f68532c519a43476b0f17
Abe Ge <yige@microsoft.com>
2018-06-09 22:12:56 +0800

    update gulp file with depandacy

--- gulpfile.js
+++ gulpfile.js
@@ -8,29 +8,29 @@ gulp.task("clean", function () {
     return gulp.src("debug/*", { read: false }).pipe(clean());
 });
 
-gulp.task("build-server", function () {
+gulp.task("build-server", ["clean"], function () {
     console.log("build typscript");
-    return gulp.src(["src/**/*.{ts,tsx}","!src/public","!src/public/**"]) // Build all ts/tsx file under src folder and exclude public
+    return gulp.src(["src/**/*.{ts,tsx}", "!src/public", "!src/public/**"]) // Build all ts/tsx file under src folder and exclude public
         .pipe(ts.createProject("tsconfig.json")())
         .pipe(gulp.dest("debug"));
 });
 
-gulp.task("build-staticResource",function(){
+gulp.task("build-staticResource", ["clean"], function () {
     console.log("build staticResource");
     gulp.src('src/public/!(less|script)/*').pipe(gulp.dest("debug/public"));
 });
 
-gulp.task("build-client",function(){
+gulp.task("build-client", ["clean"], function () {
     console.log("build client react");
     return gulp.src("src/public/script/*.{ts,tsx}")
-    .pipe(ts.createProject("tsconfig.json")())
-    .js.pipe(gulp.dest("debug/public/script"));
+        .pipe(ts.createProject("tsconfig.json")())
+        .js.pipe(gulp.dest("debug/public/script"));
 });
 
-gulp.task('default',['clean'],function(){
-    return gulp.run("build-server","build-staticResource","build-client");
+gulp.task('default', ["build-server", "build-staticResource", "build-client"], function () {
+    console.info("\x1b[32m%s\x1b[0m","Done");
 });
 
-gulp.watch("src/**/*.{ts,tsx,less,html}",['default'],function(){
+gulp.watch("src/**/*.{ts,tsx,less,html}", ['default'], function () {
     console.log("detect code change,start building...");
-})
+});
