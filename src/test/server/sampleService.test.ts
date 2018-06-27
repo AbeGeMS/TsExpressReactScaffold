a66fd268af1d48db9169f0d89cac74167077123f
Abe Ge <yige@microsoft.com>
2018-06-27 19:36:51 +0800

    Remove useless settings

--- src/test/server/sampleService.test.ts
+++ src/test/server/sampleService.test.ts
@@ -1,11 +1,6 @@
 import { SampleService } from '../../Service/sampleService'
 
 describe('jasmine test suite', () => {
-    it("1st test: a equl to true", () => {
-        let a = true;
-        expect(a).toBe(true);
-    });
-
     it('sample service test sayHi()', () => {
         let sayHelloService = new SampleService();
         let actualResult = sayHelloService.SayHi("tester");
