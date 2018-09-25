3d2323dde0c45d5bed44759b071dfdadd1d62c15
Abe Ge <yige@microsoft.com>
2018-09-25 11:49:29 +0800

    upgrade react version

--- src/public/script/view/samplePage.tsx
+++ src/public/script/view/samplePage.tsx
@@ -6,15 +6,16 @@ interface SamplePageState {
 }
 
 export class SamplePage extends React.Component<any, SamplePageState>{
+    private model: SampleModel;
+    private divRef = React.createRef<HTMLDivElement>();
     public componentWillMount() {
         this.UpdateContent();
     }
     public state = {
         content: "nothing to show...",
     }
-    private model: SampleModel;
     public render() {
-        return <div className="basis-color">{this.state.content}</div>;
+        return <div ref = {this.divRef} className="basis-color">{this.state.content}</div>;
     }
     private UpdateContent() {
         let model = new SampleModel();
