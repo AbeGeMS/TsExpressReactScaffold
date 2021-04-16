d4780adc6dd9849dba25b32e7cbe4158d4d85856
unknown <yihanms@outlook.com>
2021-04-16 16:16:41 +0800

    Add message and command

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,9 @@
+import { Request, Response, Application } from 'express';
 import express from 'express';
-// rest of the code remains same
-const app = express();
+
+const app: Application = express();
 const PORT = 3000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.get('/', (req: Request, res: Response) => res.send('Express + TypeScript Server2'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
